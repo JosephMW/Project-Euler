06e30da50d82fbdd995a1f4e09bbd119ad1c6988
Joseph Weir <2365352w@student.gla.ac.uk>
2022-08-30 17:42:46 +0100

    Solved 85!

---
+++ Problem_85(old-lost).py
@@ -0,0 +1,26 @@
+from math import *
+
+"""
+NOT FINISHED
+"""
+
+limit=2003
+num=1
+triangles = []
+
+for i in range(2,limit):
+
+    triangles = triangles + [num]
+    num = num +i
+
+
+print(triangles)
+    
+    
+limRecCount = 1000
+
+for x in range(limRecCount,1,-1):
+    #make the product function and done
+    if x product of 2 triangles:
+        print(x)
+        break
--- Problem_85.py
+++ Problem_85.py
@@ -1,26 +1,61 @@
+from gettext import find
 from math import *
 
-"""
-NOT FINISHED
-"""
+def numOfRects(cols, rows):
+    rwidth = cols
+    rheight = rows
 
-limit=2003
-num=1
-triangles = []
+    total = 0
+    numberOfSpecificDimRect = 0
+    while(rwidth > 0):
+        rheight = rows
+        while(rheight > 0):
+            numberOfSpecificDimRect = (1 + cols - rwidth) * (1 + rows - rheight)
+            total += numberOfSpecificDimRect
 
-for i in range(2,limit):
+            rheight -= 1
+        
+        rwidth -= 1
 
-    triangles = triangles + [num]
-    num = num +i
+    return total
+
+# print("HERE: " + str(numOfRects(1999,1)))
 
 
-print(triangles)
+def findClosestGrid2mil():
+    cols = 53
+    rows = 1
+
+    bestArea = 0
+    closestNumberOfRects = 2000000
+    bestCols = 0
+    bestRows = 0
+
+    while(cols <= 1999):
+        rows = 1
+
+        # print("cols: " + str(cols) + ", rows: " + str(rows) + "\n")
+        while(rows <= 53):
+            numRects = numOfRects(cols,rows)
+            distanceTo2mil = abs(numRects - 2000000)
+
+            if(distanceTo2mil < closestNumberOfRects):
+                closestNumberOfRects = distanceTo2mil
+                bestArea = cols * rows
+                bestCols = cols
+                bestRows = rows
+            
+            if(numRects > 2000000):
+                # print("breaking - cols: " + str(cols) + ", rows: " + str(rows) + "\n")
+                break
+            
+            rows += 1
+        cols += 1
     
-    
-limRecCount = 1000
+    print("closestNumberOfRects: " + str(closestNumberOfRects))
+    print("bestArea: " + str(bestArea))
+    print("bestCols: " + str(bestCols))
+    print("bestRows: " + str(bestRows))
 
-for x in range(limRecCount,1,-1):
-    #make the product function and done
-    if x product of 2 triangles:
-        print(x)
-        break
+findClosestGrid2mil()
+
