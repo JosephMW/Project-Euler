fb4f098201a3b8248544a74e6dea5b9a50ebbb5d
Joseph Weir <2365352w@student.gla.ac.uk>
2022-09-21 11:30:17 +0100

    Created C# proj for writing solutions using .NET CORE - solved Problem_1.cs

---
+++ Problem_1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Project_Euler
+{
+    class Problem_1
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            for(int i=1; i<1000; i++){
+                if(i % 3 == 0 || i % 5 == 0){
+                    // Console.WriteLine(i);
+                    sum = sum + i;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
+
+// public class Problem_1{
+
+//     public static void main(){
+//         int sum = 0
+
+//         for(int i=1; i<1000; i++){
+//             if(i % 3 == 0 || i % 5 == 0){
+//                 print(i)
+//                 sum = sum + i
+//             }
+//         }
+
+//         print(sum)
+//     }     
+
+// }
